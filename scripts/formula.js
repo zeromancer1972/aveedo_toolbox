a60787580d1439a1f7d06a9a9bee4123d6e1062e
Oliver Busse <obusse@googlemail.com>
2017-12-22 22:22:38 +0100

    some DbColumn and DbLookup

---
+++ scripts/formula.js
@@ -0,0 +1,63 @@
+var Formula = {
+    /* @param obj:String, viewName:String, key:any, colNo:int, unique:boolean */
+    DbColumn: function(obj, viewName, key, colNo, unique) {
+        try {
+            /* get the data source */
+            var db = API.getDatabaseForObject(obj);
+            /* get the view */
+            var view = db.getView(viewName);
+            /* get all documents by the key */
+            var col = view.getAllEntries(key, true);
+            /* build the return list */
+            var list = [];
+            var ent = col.getFirstEntry();
+            while (ent != null) {
+                var col = ent.getColumnValues();
+                var colVal = col.elementAt(colNo - 1);
+                if (!unique || !inArray(colVal, list)) {
+                    list.push();
+                }
+                ent = col.getNextEntry(ent);
+            }
+            /* return the list */
+            return list;
+        } catch (e) {
+            API.log().error(e.toString());
+            return "ERROR - check log";
+        }
+    },
+    /* @param obj:String, viewName:String, key:any, colNo:int */
+    DbLookup: function(obj, viewName, key, colNo) {
+        try {
+            /* get the data source */
+            var db = API.getDatabaseForObject(obj);
+            /* get the view */
+            var view = db.getView(viewName);
+            /* get all documents by the key */
+            var col = view.getAllEntriesByKey(key, true);
+            /* build the return list */
+            var list = [];
+            var ent = col.getFirstEntry();
+            while (ent != null) {
+                list.push(ent.getColumnValues().elementAt(colNo - 1));
+                ent = col.getNextEntry(ent);
+            }
+            /* return the list */
+            return list;
+
+        } catch (e) {
+            API.log().error(e.toString());
+            return "ERROR - check log";
+        }
+    },
+    /* aux function */
+    inArray: function(target, array) {
+        /* Caching array.length doesn't increase the performance of the for loop on V8 (and probably on most of other major engines) */
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] === target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
